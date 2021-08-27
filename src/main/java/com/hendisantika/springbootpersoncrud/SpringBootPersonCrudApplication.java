6b24acfad7c1de123aba9791baf90c3a613a277c
Hendi Santika <hendisantika@gmail.com>
2021-08-28 06:10:59 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootpersoncrud/SpringBootPersonCrudApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootpersoncrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPersonCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPersonCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hendisantika/springbootpersoncrud/SpringBootPersonCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootpersoncrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPersonCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
