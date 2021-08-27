464ca2b3a1a79aad8b5c87cea8eaa10cf1ced0c7
Hendi Santika <hendisantika@gmail.com>
2021-08-28 06:17:15 +0700

    #4 - Add Person Entity

---
+++ src/main/java/com/hendisantika/springbootpersoncrud/entity/Person.java
@@ -0,0 +1,29 @@
+package com.hendisantika.springbootpersoncrud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-person-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 28/08/21
+ * Time: 06.16
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String telephone;
+}
