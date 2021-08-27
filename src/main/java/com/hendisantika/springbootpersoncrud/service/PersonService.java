548aeeee396248643480297669a3b0170162b30b
Hendi Santika <hendisantika@gmail.com>
2021-08-28 06:22:03 +0700

    #8 - Add findById in Person Service

--- src/main/java/com/hendisantika/springbootpersoncrud/service/PersonService.java
+++ src/main/java/com/hendisantika/springbootpersoncrud/service/PersonService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,5 +24,9 @@ public class PersonService {
 
     public List<Person> list() {
         return (List<Person>) personRepository.findAll();
+    }
+
+    public Optional<Person> findById(int id) {
+        return personRepository.findById(id);
     }
 }
