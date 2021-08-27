7c1916defd7bbb2da3cf254f931b3e0d0b31969c
Hendi Santika <hendisantika@gmail.com>
2021-08-28 06:31:39 +0700

    #12 - Add findById in PersonControler

--- src/main/java/com/hendisantika/springbootpersoncrud/controller/PersonControler.java
+++ src/main/java/com/hendisantika/springbootpersoncrud/controller/PersonControler.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -27,4 +28,11 @@ public class PersonControler {
         model.addAttribute("persons", personService.list());
         return "index";
     }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable int id, Model model) {
+        model.addAttribute("person", personService.findById(id));
+        return "form";
+    }
+
 }
