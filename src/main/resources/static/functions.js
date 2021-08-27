4d3bbcf61c5863a88ab7ad45809679dcb765fbb9
Hendi Santika <hendisantika@gmail.com>
2021-08-28 06:42:41 +0700

    #15 - Add functions.js file

---
+++ src/main/resources/static/functions.js
@@ -0,0 +1,27 @@
+function deletePerson(id) {
+    console.log(id);
+    swal({
+        title: "Are you sure to Delete?",
+        text: "Once deleted it cannot be restored!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((OK) => {
+            if (OK) {
+                $.ajax({
+                    url: "/delete/" + id,
+                    success: function (res) {
+                        console.log(res);
+                    },
+                });
+                swal("Poof! Record deleted!", {
+                    icon: "success",
+                }).then((ok) => {
+                    if (ok) {
+                        location.href = "/";
+                    }
+                });
+            }
+        });
+}
